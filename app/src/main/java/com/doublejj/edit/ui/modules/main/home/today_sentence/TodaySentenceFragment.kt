f7b5f58284143840c1baf7014d06fbbd93d13cc6
yezji <yeji.dev@gmail.com>
2021-03-11 23:57:26 +0900

    feat: Add today sentence card

---
+++ app/src/main/java/com/doublejj/edit/ui/modules/main/home/today_sentence/TodaySentenceFragment.kt
@@ -0,0 +1,29 @@
+package com.doublejj.edit.ui.modules.main.home.today_sentence
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.doublejj.edit.R
+import com.doublejj.edit.databinding.TodaySentenceFragmentBinding
+
+class TodaySentenceFragment : Fragment() {
+    private lateinit var binding: TodaySentenceFragmentBinding
+    private lateinit var viewModel: TodaySentenceViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.today_sentence_fragment, container, false)
+        viewModel = ViewModelProvider(this).get(TodaySentenceViewModel::class.java)
+
+        binding.todaySentenceViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
+}
