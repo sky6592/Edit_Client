f9bb97fb6014f632eb2b8de835d96a7cd54b5203
ihaneul <789_skymert@naver.com>
2021-03-15 21:38:53 +0900

    add : splash - xml

--- app/src/main/java/com/doublejj/edit/ui/modules/main/walkthrough/WalkThroughActivity.kt
+++ app/src/main/java/com/doublejj/edit/ui/modules/main/walkthrough/WalkThroughActivity.kt
@@ -1,15 +1,19 @@
 package com.doublejj.edit.ui.modules.main.walkthrough
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.doublejj.edit.R
 import com.doublejj.edit.data.models.walkthrough.WalkThroughModel
 import com.doublejj.edit.databinding.ActivityWalkThroughBinding
-import com.doublejj.edit.ui.modules.main.login.LogInActivity
+import com.doublejj.edit.ui.modules.main.signin.SignInActivity
 import com.doublejj.edit.ui.modules.main.signup.SignUpActivity
 import pl.pzienowicz.autoscrollviewpager.AutoScrollViewPager
 
 class WalkThroughActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityWalkThroughBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,7 +26,7 @@ class WalkThroughActivity : AppCompatActivity() {
             WalkThroughModel(R.drawable.walkthrough_img3)
         )
 
-        mBinding.autoViewPagerWalkThrough.adapter = WalkThroughAdapter(this,imgModel)
+        mBinding.autoViewPagerWalkThrough.adapter = WalkThroughAdapter(this, imgModel)
         mBinding.autoViewPagerWalkThrough.setInterval(3000)
         mBinding.autoViewPagerWalkThrough.setDirection(AutoScrollViewPager.Direction.RIGHT)
         mBinding.autoViewPagerWalkThrough.setCycle(true)
@@ -30,8 +34,6 @@ class WalkThroughActivity : AppCompatActivity() {
         mBinding.autoViewPagerWalkThrough.setSlideBorderMode(AutoScrollViewPager.SlideBorderMode.TO_PARENT)
         mBinding.autoViewPagerWalkThrough.startAutoScroll()
         mBinding.dotsIndicatorWalkThorugh.setViewPager(mBinding.autoViewPagerWalkThrough)
-
-
     }
 
     fun onMoveSignUp() {
@@ -39,6 +41,6 @@ class WalkThroughActivity : AppCompatActivity() {
     }
 
     fun onMoveLogIn() {
-        return startActivity(Intent(this, LogInActivity::class.java))
+        return startActivity(Intent(this, SignInActivity::class.java))
     }
 }
