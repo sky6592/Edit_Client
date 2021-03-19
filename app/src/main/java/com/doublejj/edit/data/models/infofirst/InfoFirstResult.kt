16d2b1ce680e45f4593a3bb1824f0dea7c1e13ad
ihaneul <789_skymert@naver.com>
2021-03-20 05:10:53 +0900

    add : InfoFirstActivity API complete

---
+++ app/src/main/java/com/doublejj/edit/data/models/infofirst/InfoFirstResult.kt
@@ -0,0 +1,7 @@
+package com.doublejj.edit.data.models.infofirst
+
+import com.google.gson.annotations.SerializedName
+
+data class InfoFirstResult(
+    @SerializedName("duplicationCheck") val duplicationCheck: String
+)
