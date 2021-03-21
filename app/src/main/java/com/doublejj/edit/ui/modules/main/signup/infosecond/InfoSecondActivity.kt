5e54d5608433f27fe1c757fa98293e1c3e74c0c9
ihaneul <789_skymert@naver.com>
2021-03-22 05:31:51 +0900

    add : InfoSecondActivity 정규식

--- app/src/main/java/com/doublejj/edit/ui/modules/main/signup/infosecond/InfoSecondActivity.kt
+++ app/src/main/java/com/doublejj/edit/ui/modules/main/signup/infosecond/InfoSecondActivity.kt
@@ -1,12 +1,16 @@
 package com.doublejj.edit.ui.modules.main.signup.infosecond
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.doublejj.edit.R
 import com.doublejj.edit.data.api.services.infosecond.InfoSecondService
@@ -26,14 +30,19 @@ class InfoSecondActivity : AppCompatActivity(), InfoSecondView {
     private var mEmailSpacingFlag: Boolean = false
     private var mPwSpacingFlag: Boolean = false
     private var mRePwSpacingFlag: Boolean = false
+    private var mEmailBtnFlag: Boolean = false
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_info_second)
 
-        val emailPattern = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+$"
+        val emailPattern = "^[a-zA-Z0-9]+@[a-zA-Z0-9]$"
+        val emailPatternTest = android.util.Patterns.EMAIL_ADDRESS
+
         //문자(한글,영어), 숫자, 특수문자 중 2가지 포함(8-15자)
-        val pwPattern = "^(?=.*[a-zA-Z0-9가-힣])(?=.*[a-zA-Z!@#\$%^&*])(?=.*[0-9!@#\$%^&*]).{8,15}$"
+        val pwPattern =
+            "^(?=.*[a-zA-Z0-9가-힣])(?=.*[a-zA-Z~!`@#\$%^&*()_+=])(?=.*[0-9!@#\$%^&*]).{8,15}$"
 
         //이메일 입력
         mBinding.etEmailInfoSecond.addTextChangedListener(object : TextWatcher {
@@ -41,37 +50,65 @@ class InfoSecondActivity : AppCompatActivity(), InfoSecondView {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                try {
-
-
-                } catch (e: Exception) {
-                    Log.d("exception", e.toString())
-                }
             }
 
+            @SuppressLint("ResourceAsColor")
             override fun afterTextChanged(s: Editable?) {
                 if (s != null) {
+                    //엔터 입력 : 다른 내용 입력 되어있는지 확인
+//                    if (s.toString().contains("\n")) {
+//                        //s.toString().trim()
+//                        if (mPwFlag && mRePwFlag) {
+//                            mBinding.btnInfoSecond.setBackgroundResource(R.color.purple)
+//                        }
+//                    }
                     //띄어쓰기 정규식
-                    if (s.toString().contains(" ")) {
+                    if (s.toString().contains(" ") || s.toString().isEmpty()) {
+                        mBinding.btnInfoSecond.setBackgroundResource(R.color.very_light_pink)
+                        mBinding.tvEmailCaptionInfoFirst.setTextColor(R.color.purple)
                         mBinding.tvEmailCaptionInfoFirst.text =
                             getString(R.string.tv_caption_spacing_info)
                         mEmailSpacingFlag = true
+                        mEmailFlag = false
                     } else {
                         mEmailSpacingFlag = false
                     }
 
+
                     //이메일 정규식 - 중복확인을 누르세요!
-                    if (s.matches(emailPattern.toRegex()) && !mEmailSpacingFlag) {
+                    if (emailPatternTest.matcher(s.toString()).matches()) {
+                        mBinding.tvEmailCaptionInfoFirst.setTextColor(R.color.purple)
                         mBinding.tvEmailCaptionInfoFirst.text =
                             getString(R.string.tv_click_caption_info)
+                        mEmailBtnFlag = true
                     }
                     //입력 완료여부 정규식
                     if (mEmailFlag && mPwFlag && mRePwFlag) {
                         mBinding.btnInfoSecond.setBackgroundResource(R.color.purple)
                     }
+                    Log.d("sky", "이메일 엔 : $mEmailFlag, $mPwFlag, $mRePwFlag")
+
                 }
             }
         })
+
+        //이메일 중복확인
+        mBinding.btnDoubleCheck.setOnClickListener {
+            if (mEmailBtnFlag) {
+                mBinding.etEmailInfoSecond.toString().replace(" ", "")
+
+                //이메일 중복 API
+                val email = mBinding.etEmailInfoSecond.text.toString()
+                val postRequest = InfoSecondRequest(email = email)
+                //다이얼로그 넣어야함
+                InfoSecondService(this).tryPostEmail(postRequest)
+                mEmailBtnFlag = false
+            } else {
+                mBinding.tvEmailCaptionInfoFirst.setTextColor(R.color.purple)
+                mBinding.tvEmailCaptionInfoFirst.text =
+                    getString(R.string.tv_email_result_wrong_info)
+            }
+        }
 
         //비밀번호 입력
         mBinding.etPwInfoSecond.addTextChangedListener(object : TextWatcher {
@@ -79,36 +116,56 @@ class InfoSecondActivity : AppCompatActivity(), InfoSecondView {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                //띄어쓰기 정규식
-                try {
-                    if (s.toString().contains(" ")) {
+            }
+
+            @SuppressLint("ResourceAsColor")
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    //엔터 입력 : 다른 내용 입력 되어있는지 확인
+                    if (s.toString().contains("\n")) {
+                        //s.toString().trim()
+                        if (mEmailFlag && mRePwFlag) {
+                            mBinding.btnInfoSecond.setBackgroundResource(R.color.purple)
+                        }
+                    }
+                    //띄어쓰기 정규식
+                    if (s.toString().contains(" ") || s.toString().isEmpty()) {
                         mBinding.tvPwCaptionInfoSecond.text =
                             getString(R.string.tv_caption_spacing_info)
                         mPwSpacingFlag = true
-                    }
-                    if (!s.toString().contains(" ")) {
+                        mPwFlag = false
+                        mBinding.btnInfoSecond.setBackgroundResource(R.color.very_light_pink)
+                    } else {
                         mPwSpacingFlag = false
+                        mBinding.tvPwCaptionInfoSecond.text = getString(R.string.tv_pw_caption_info)
                     }
 
-                } catch (e: Exception) {
-                    Log.d("exception", e.toString())
-                }
-            }
 
-            override fun afterTextChanged(s: Editable?) {
-                if (s != null) {
+                    //비밀번호 일치여부
+                    if (mBinding.etRePwInfoFirst.text.toString() == s.toString()) {
+                        mBinding.tvPwCaptionInfoSecond.text = ""
+                        mRePwFlag = true
+                    } else {
+                        mBinding.btnInfoSecond.setBackgroundResource(R.color.very_light_pink)
+
+                        mRePwFlag = false
+                    }
                     //비밀번호 정규식
-                    if (s.toString().matches(pwPattern.toRegex()) && !mEmailSpacingFlag) {
+                    if (s.toString().matches(pwPattern.toRegex())) {
                         mBinding.tvPwCaptionInfoSecond.text = ""
                         mPwFlag = true
                     } else {
+                        mBinding.tvPwCaptionInfoSecond.setTextColor(R.color.purple)
                         mBinding.tvPwCaptionInfoSecond.text =
                             getString(R.string.tv_pw_wrong_caption_info)
+                        mPwFlag = false
                     }
                     //입력 완료여부 정규식
                     if (mEmailFlag && mPwFlag && mRePwFlag) {
                         mBinding.btnInfoSecond.setBackgroundResource(R.color.purple)
                     }
+                    Log.d("sky", "비번 엔터 : $mEmailFlag, $mPwFlag, $mRePwFlag")
+
                 }
             }
         })
@@ -119,80 +176,112 @@ class InfoSecondActivity : AppCompatActivity(), InfoSecondView {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                //띄어쓰기 정규식
-                try {
-                    if (s.toString().contains(" ")) {
+            }
+
+            @SuppressLint("ResourceAsColor")
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    //엔터 입력 : 다른 내용 입력 되어있는지 확인
+                    if (s.toString().contains("\n")) {
+                        //s.toString().replace("\n", "")
+                        //s.toString().trim()
+
+                        if (mPwFlag && mRePwFlag) {
+                            mBinding.btnInfoSecond.setBackgroundResource(R.color.purple)
+                        }
+                    }
+                    //띄어쓰기 정규식
+                    if (s.toString().contains(" ") || s.toString().isEmpty()) {
+                        mBinding.tvRePwCaptionInfoFirst.setTextColor(R.color.purple)
                         mBinding.tvRePwCaptionInfoFirst.text =
                             getString(R.string.tv_caption_spacing_info)
                         mRePwSpacingFlag = true
-                    }
-                    if (!s.toString().contains(" ")) {
+                        mRePwFlag = false
+                        mBinding.btnInfoSecond.setBackgroundResource(R.color.very_light_pink)
+                    } else {
                         mRePwSpacingFlag = false
+                        mBinding.tvRePwCaptionInfoFirst.setTextColor(R.color.very_light_pink)
+
                     }
 
-                } catch (e: Exception) {
-                    Log.d("exception", e.toString())
-                }
-            }
+                    Log.d("sky", mBinding.etPwInfoSecond.text.toString() + ", " + s.toString())
 
-            override fun afterTextChanged(s: Editable?) {
-                if (s != null) {
                     //비밀번호 일치여부
-                    if (mBinding.etPwInfoSecond.text == s) {
-                        mBinding.tvRePwCaptionInfoFirst.text =
-                            getString(R.string.tv_re_pw_caption_info)
+                    if (mBinding.etPwInfoSecond.text.toString() == s.toString()) {
+                        mBinding.tvRePwCaptionInfoFirst.text = ""
                         mRePwFlag = true
 
                     } else {
-                        mBinding.tvRePwCaptionInfoFirst.text = getString(R.string.hint_re_pw_info)
+                        mBinding.btnInfoSecond.setBackgroundResource(R.color.very_light_pink)
+                        mBinding.tvRePwCaptionInfoFirst.text =
+                            getString(R.string.tv_re_pw_caption_wrong_info)
+                        mRePwFlag = false
+                    }
+                    //비밀번호 정규식
+                    if (s.toString().matches(pwPattern.toRegex())) {
+                        mBinding.tvRePwCaptionInfoFirst.text = ""
+                        mRePwFlag = true
+                    } else {
+                        mBinding.tvRePwCaptionInfoFirst.text =
+                            getString(R.string.tv_pw_wrong_caption_info)
+                        mRePwFlag = false
                     }
                     //입력 완료여부 정규식
                     if (mEmailFlag && mPwFlag && mRePwFlag) {
                         mBinding.btnInfoSecond.setBackgroundResource(R.color.purple)
                     }
+                    Log.d("sky", "비번 재 엔터 : $mEmailFlag, $mPwFlag, $mRePwFlag")
                 }
             }
 
         })
 
 
-        //이메일 중복확인
-        mBinding.btnDoubleCheck.setOnClickListener {
-            val email = mBinding.etEmailInfoSecond.text.toString()
-            val postRequest = InfoSecondRequest(email = email)
-            //다이얼로그 넣어야함
-            InfoSecondService(this).tryPostEmail(postRequest)
-        }
-
         //다음으로 버튼 클릭
         mBinding.btnInfoSecond.setOnClickListener {
+            Log.d("sky", "btnInfoSecond - In")
+            Log.d(
+                "sky",
+                "중복버튼 :" + mEmailFlag + ", " + mPwFlag + ", " + mRePwFlag
+            )
             if (mEmailFlag && mPwFlag && mRePwFlag) {
                 mEmailFlag = false
                 mPwFlag = false
                 mRePwFlag = false
                 startActivity(Intent(this, JobGroupActivity::class.java))
+                finish()
             } else {
                 //다이얼로그 : 입력을 다시 확인해주세요!
             }
+
         }
     }
 
     //네트워크 통신
+    @SuppressLint("ResourceType")
     override fun onPostInfoSecondSuccess(response: InfoSecondResponse) {
         //다이얼로그 해제 넣기
         Log.d("sky", "onPostInfoSecondSuccess - api성공")
-        mEmailFlag = true
-
         runOnUiThread {
-            Toast.makeText(this, "onPostInfoSecondSuccess - API성공", Toast.LENGTH_SHORT).show()
+            mEmailFlag = true
+            //이메일 입력 감사합니다
+            mBinding.tvEmailCaptionInfoFirst.text = ""
+
+            //입력 완료여부 정규식
+            if (mEmailFlag && mPwFlag && mRePwFlag) {
+                mBinding.btnInfoSecond.setBackgroundResource(R.color.purple)
+            }
         }
     }
 
+    @SuppressLint("ResourceType")
     override fun onPostInfoSecondFailure(message: String) {
         //다이얼로그 해제 넣기
-        Log.d("sky", "onPostInfoSecondFailure - api실")
+        Log.d("sky", "onPostInfoSecondFailure - api실패")
 
-        Toast.makeText(this, "onPostInfoSecondFailure - API실패", Toast.LENGTH_SHORT).show()
+        runOnUiThread {
+            mBinding.tvEmailCaptionInfoFirst.text = getString(R.string.tv_email_fail_info)
+            mBinding.tvEmailCaptionInfoFirst.setTextColor(R.color.purple)
+        }
     }
-
 }
