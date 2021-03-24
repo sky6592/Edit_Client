be37b382f350ad226d3432306d87621c3f2d6ee9
yezji <yeji.dev@gmail.com>
2021-03-24 23:34:55 +0900

    feat: Add get my profile API

--- app/src/main/java/com/doublejj/edit/data/api/interceptors/XAccessTokenInterceptor.kt
+++ app/src/main/java/com/doublejj/edit/data/api/interceptors/XAccessTokenInterceptor.kt
@@ -1,6 +1,7 @@
 package com.doublejj.edit.data.api.interceptors
 
 import com.doublejj.edit.ApplicationClass
+import com.doublejj.edit.ApplicationClass.Companion.sSharedPreferences
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -8,7 +9,9 @@ import okhttp3.Response
 class XAccessTokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder: Request.Builder = chain.request().newBuilder()
-        val jwtToken: String? = ApplicationClass.sSharedPreferences.getString(ApplicationClass.X_ACCESS_TOKEN, null)
+        // TODO : 테스트 jwt 지우기
+        val jwtToken: String? = sSharedPreferences.getString(ApplicationClass.X_ACCESS_TOKEN, "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjksInJvbGUiOiJNRU5URUUiLCJpYXQiOjE2MTY1OTAwODAsImV4cCI6MTYxNzc5OTY4MH0.ovMXDKHhQxHqSu2Ydzt-1CPbgmp5KtHNarEB63a7AX0")
+//        val jwtToken: String? = sSharedPreferences.getString(ApplicationClass.X_ACCESS_TOKEN, null)
         if (jwtToken != null) {
             builder.addHeader("X-ACCESS-TOKEN", jwtToken)
         }
--- app/src/main/java/com/doublejj/edit/ui/modules/main/myedit/MyeditFragment.kt
+++ app/src/main/java/com/doublejj/edit/ui/modules/main/myedit/MyeditFragment.kt
@@ -3,19 +3,28 @@ package com.doublejj.edit.ui.modules.main.myedit
 import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.doublejj.edit.ApplicationClass
+import com.doublejj.edit.ApplicationClass.Companion.USER_COLOR
+import com.doublejj.edit.ApplicationClass.Companion.USER_EMOTION
+import com.doublejj.edit.ApplicationClass.Companion.USER_NICKNAME
+import com.doublejj.edit.ApplicationClass.Companion.USER_POSITION
+import com.doublejj.edit.ApplicationClass.Companion.sSharedPreferences
 import com.doublejj.edit.R
+import com.doublejj.edit.data.api.services.myedit_info.ProfileInfoService
+import com.doublejj.edit.data.api.services.myedit_info.ProfileInfoView
+import com.doublejj.edit.data.models.myedit_info.ProfileInfoResponse
 import com.doublejj.edit.databinding.MyeditFragmentBinding
 import com.doublejj.edit.ui.modules.main.myedit.settings.SettingsActivity
 import com.doublejj.edit.ui.modules.main.myedit.settings.profile.ProfileActivity
+import com.doublejj.edit.ui.utils.snackbar.CustomSnackbar
+import com.google.android.material.snackbar.Snackbar
 
-class MyeditFragment : Fragment() {
+class MyeditFragment : Fragment(), ProfileInfoView {
     private val TAG: String = javaClass.simpleName.toString()
     private lateinit var binding: MyeditFragmentBinding
     private lateinit var viewModel: MyeditViewModel
@@ -30,7 +39,9 @@ class MyeditFragment : Fragment() {
         binding.myeditViewModel = viewModel
         binding.lifecycleOwner = this
 
-        when (ApplicationClass.sSharedPreferences.getString(ApplicationClass.USER_POSITION, "MENTEE")) {
+        ProfileInfoService(this).tryGetProfileInfo()
+
+        when (sSharedPreferences.getString(USER_POSITION, "MENTEE")) {
             "MENTEE" -> {
                 binding.tvPosition.text = "멘티님"
                 binding.llBtnMenuSentence.visibility = View.VISIBLE
@@ -60,18 +71,8 @@ class MyeditFragment : Fragment() {
 
         /** menu buttons **/
         binding.ibMenuProfile.setOnClickListener {
-            // TODO : 내 정보 페이지
-            Log.d(TAG, "clicked profile")
+            // 내 정보 페이지
             val sendIntent = Intent(activity, ProfileActivity::class.java)
-            // TODO : 프로필 받아서 테스트 교체
-            val nickName = "테스트"
-            val userRole = "멘티님"
-            val emotionName = "relief"
-            val colorName = "purple"
-            sendIntent.putExtra("nickName", nickName)
-            sendIntent.putExtra("userRole", userRole)
-            sendIntent.putExtra("emotionName", emotionName)
-            sendIntent.putExtra("colorName", colorName)
             startActivity(sendIntent)
         }
         binding.ibMenuCoin.setOnClickListener {
@@ -96,4 +97,23 @@ class MyeditFragment : Fragment() {
         return binding.root
     }
 
+    override fun onProfileInfoSuccess(response: ProfileInfoResponse) {
+        if (response.isSuccess) {
+            val editor = sSharedPreferences.edit()
+            editor.putString(USER_NICKNAME, response.result.nickName)
+            editor.putString(USER_EMOTION, response.result.emotionName)
+            editor.putString(USER_COLOR, response.result.colorName)
+            editor.putString(USER_POSITION, response.result.userRole)
+            editor.commit()
+            editor.apply()
+
+            binding.tvNickname.text = response.result.nickName
+            binding.tvPosition.text = (requireContext().applicationContext as ApplicationClass).getPostionToString(response.result.userRole)
+            binding.ivProfile.setImageResource((requireContext().applicationContext as ApplicationClass).getCharacterResId(response.result.colorName, response.result.emotionName))
+        }
+    }
+
+    override fun onProfileInfoFailure(message: String) {
+        CustomSnackbar.make(binding.root, message, Snackbar.LENGTH_SHORT)
+    }
 }
