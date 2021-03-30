884316727b6b3f9a0b3874bb1c2b9eeeb3595f61
ihaneul <789_skymert@naver.com>
2021-03-30 09:38:44 +0900

    rebase main

--- app/src/main/java/com/doublejj/edit/data/api/interceptors/XAccessTokenInterceptor.kt
+++ app/src/main/java/com/doublejj/edit/data/api/interceptors/XAccessTokenInterceptor.kt
@@ -10,7 +10,6 @@ class XAccessTokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder: Request.Builder = chain.request().newBuilder()
         // TODO : 테스트 jwt 지우기
-//        val jwtToken: String? = sSharedPreferences.getString(ApplicationClass.X_ACCESS_TOKEN, "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjksInJvbGUiOiJNRU5URUUiLCJpYXQiOjE2MTY1OTAwODAsImV4cCI6MTYxNzc5OTY4MH0.ovMXDKHhQxHqSu2Ydzt-1CPbgmp5KtHNarEB63a7AX0")
         val jwtToken: String? = sSharedPreferences.getString(ApplicationClass.X_ACCESS_TOKEN, null)
 
         if (jwtToken != null) {
